34760d23cb3ec7a56b27b1dc315e25089822cfb8
WasACute <marattukasov5@gmail.com>
2026-05-09 19:01:51 +0400

    Бд подключена

--- src/main/java/ru/glassgo/controller/AuthController.java
+++ src/main/java/ru/glassgo/controller/AuthController.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.sql.*;
@@ -30,5 +29,6 @@ public class AuthController extends HttpServlet {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
         PrintWriter out = resp.getWriter();
+
     }
 }
